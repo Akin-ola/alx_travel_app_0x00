38160184228a74a05669673402f1b44480ee6328
Akin-ola <idris_akinola@yahoo.com>
2025-07-09 09:18:31 +0100

    Updated the settings to use django environ in managonging secret key and other sensitive credentials

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -1,3 +1,5 @@
+import os
+import environ 
 """
 Django settings for alx_travel_app project.
 
@@ -15,15 +17,23 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialise environment variables
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+# Read from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^4&k)z^p*jp2gb$l(=tq&*pyih-fy#p12+-l9nni)%r2fqr8xf'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restframework',
+    'django-cors-headers',
+
 ]
 
 MIDDLEWARE = [
@@ -74,8 +87,12 @@ WSGI_APPLICATION = 'alx_travel_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.MySQL',
+        'NAME': env('db_name'),
+        'USER': env('db_user'),
+        'HOST': env('db_host'),
+        'PORT': env('db_port'),
+        'PASSWORD': env('db_password')
     }
 }
 
